276af659f249f658a3d498e1627007583242476b
xabier_laibarra <xlaibarra@gmail.com>
2026-03-23 13:43:11 +0100

    roman-to-integer.js debugged

--- code_completion/roman-to-integer.js
+++ code_completion/roman-to-integer.js
@@ -15,6 +15,7 @@ return s.match(/^[IVXLCDM]+$/) !== null;
 };
 
 rl.question('Enter a Roman numeral: ', (answer) => {
+	// If the user types 'help', provide a brief explanation of Roman numerals
 
 	if (answer == 'help') {
 		console.log('Roman numerals are a number system that originated in ancient Rome. They use combinations of letters from the Latin alphabet to represent values. The basic symbols are: I (1), V (5), X (10), L (50), C (100), D (500), and M (1000). To convert a Roman numeral to an integer, you can use the following rules: If a smaller numeral is placed before a larger numeral, it is subtracted from the larger numeral. If a smaller numeral is placed after a larger numeral, it is added to the larger numeral. For example, IV is 4 (5 - 1) and VI is 6 (5 + 1).');
@@ -33,12 +34,28 @@ rl.question('Enter a Roman numeral: ', (answer) => {
 
 
 function romanToInt(s) {
-  let romanToInt= {
-	'I': 1,
-	'V': 5,
-	'X': 10,
-	'L': 50,
-	'C': 100,
-	'D': 500,
-	'M': 1000
-  };
+	const romanMap = {
+	  'I': 1,
+	  'V': 5,
+	  'X': 10,
+	  'L': 50,
+	  'C': 100,
+	  'D': 500,
+	  'M': 1000
+	};
+
+	let total = 0;
+	for (let i = 0; i < s.length; i++) {
+	  const current = romanMap[s[i]];
+	  const next = romanMap[s[i + 1]];
+
+	  // If the current value is less than the next value, subtract it
+	  if (next && current < next) {
+		total -= current;
+	  } else {
+		total += current;
+	  }
+	}
+
+	return total;
+  }

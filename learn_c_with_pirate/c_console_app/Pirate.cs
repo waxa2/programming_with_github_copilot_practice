16c705469ae98c0705f596675b0762ab9e18163e
xabier_laibarra <xlaibarra@gmail.com>
2026-03-24 12:36:46 +0100

    create Pirate class

--- learn_c_with_pirate/c_console_app/Pirate.cs
+++ learn_c_with_pirate/c_console_app/Pirate.cs
@@ -2,12 +2,22 @@ using System;
 
 namespace c_console_app
 {
-    class Program
+    class Pirate
     {
-        static void Main(string[] args)
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Ship { get; set; }
+
+        public Pirate(string name, int age, string ship)
         {
-            Console.WriteLine("Ahoy, welcome to the PiratesExample application!");
-            // Additional application logic goes here
+            Name = name;
+            Age = age;
+            Ship = ship;
+        }
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Ahoy! My name is {Name}, I'm {Age} years old, and I sail on the {Ship}.");
         }
     }
 }
